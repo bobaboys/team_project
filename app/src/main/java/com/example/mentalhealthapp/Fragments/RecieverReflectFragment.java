f0b9483280ed6d5e45a0c608b516b14e745c8a29
Elena Gray <egray@fb.com>
2019-07-16 17:34:15 -0700

    elena day 1

---
+++ app/src/main/java/com/example/mentalhealthapp/Fragments/RecieverReflectFragment.java
@@ -0,0 +1,30 @@
+package com.example.mentalhealthapp.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mentalhealthapp.R;
+
+public class RecieverReflectFragment extends Fragment {
+
+    protected TextView testChat;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reciever_reflect, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        testChat = view.findViewById(R.id.recreflectTest);
+
+    }
+}
