7a570d3445c4e40a3d3367dbb06796bba96e0df6
Michelle Xu <michellecxu@fb.com>
2019-07-22 09:52:39 -0700

    Alex was having some problems with the edit button under the helper profile page, but it works on my computer so just committing again just in case, also fixed the default home page to be the search page instead of the chats page

--- app/src/main/java/com/example/mentalhealthapp/HelperDetails.java
+++ app/src/main/java/com/example/mentalhealthapp/HelperDetails.java
@@ -108,7 +108,7 @@ public class HelperDetails extends AppCompatActivity {
     }
 
     public  void startChatApp(Context context){
-        APP_ID = context.getString(R.string.APP_ID);
+        APP_ID = context.getString(R.string.APP_CHAT_ID);
         SendBird.init(APP_ID, context);
         //This api is called using only our secret App ID.
     }
