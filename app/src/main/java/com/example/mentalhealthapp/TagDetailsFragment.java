6b6f5d5680e1ef4f7b5c9094475ef103761797a2
alexgz9851 <zzz12300212@gmail.com>
2019-07-22 15:21:36 -0700

    Tag details fragment populated, link with more info clickable

--- app/src/main/java/com/example/mentalhealthapp/TagDetailsFragment.java
+++ app/src/main/java/com/example/mentalhealthapp/TagDetailsFragment.java
@@ -1,16 +1,20 @@
 package com.example.mentalhealthapp;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mentalhealthapp.model.Tag;
 
 
 public class TagDetailsFragment extends Fragment {
-
+    Tag tag;
+    TextView title, description, link;
     public TagDetailsFragment() {
         // Required empty public constructor
     }
@@ -25,5 +29,20 @@ public class TagDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tag_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        Bundle bundle = getArguments();
+        tag = (Tag) bundle.getParcelable("tag");
+        title = view.findViewById(R.id.tv_tag_title);
+        description = view.findViewById(R.id.tv_tag_details_content);
+        link = view.findViewById(R.id.tv_tag_link);
+
+        title.setText(tag.getString("Tag"));
+        description.setText(tag.getString("Description"));
+        link.setText(tag.getString("Link"));
+
+
     }
 }
