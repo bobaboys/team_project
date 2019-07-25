45aed47455eceb24571d6da2500f78ef2da45dd5
Elena Gray <egray@fb.com>
2019-07-25 11:06:01 -0700

    constants class, start using and adding to it

---
+++ app/src/main/java/com/example/mentalhealthapp/models/Constants.java
@@ -0,0 +1,23 @@
+package com.example.mentalhealthapp.models;
+
+public final class Constants {
+
+    //PARSE KEYS:
+    //Classes:
+    public final String HELPER_TAGS_PARSE_OBJECT = "HelperTags";
+    public final String USER_PARSE_CLASS = "user";
+
+    //Fields:
+    public final String HELPER_BIO_FIELD = "helperBio";
+    public final String HELPER_FIELD = "helper";
+    public final String AVATAR_FIELD = "avatar";
+
+    //REQUEST CODES:
+    public final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    static final int CHOOSE_AVATAR_REQUEST = 333;
+
+    //OTHER:
+    public final String TAG = "tag: ";
+    public final String CLICKED_AVATAR_KEY = "clicked_avatar";
+
+}
