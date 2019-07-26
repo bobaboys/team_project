f345f080cfec3b88ae1e9cb9e68307060fed7ed4
Michelle Xu <michellecxu@fb.com>
2019-07-26 16:09:10 -0700

    possible to create journal entries, now need to work on creating a journal class on the parseserver and connecting entries to the server so that the entries for each person is saved on the server

--- app/src/main/java/com/example/mentalhealthapp/Fragments/ReceiverJournalEditFragment.java
+++ app/src/main/java/com/example/mentalhealthapp/Fragments/ReceiverJournalEditFragment.java
@@ -7,24 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.example.mentalhealthapp.R;
 
-public class HelperReflectFragment extends Fragment {
+public class ReceiverJournalEditFragment extends Fragment {
+    protected EditText etItemText;
+    protected int position;
 
-    protected TextView testReflect;
-
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        return inflater.inflate(R.layout.fragment_receiver_journaledit, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        testReflect = view.findViewById(R.id.reflectTest);
 
+//        etItemText = (EditText) view.findViewById(R.id.etItemText);
+//        // set the text field's content from the intent
+//        etItemText.setText(getIntent().getStringExtra(ITEM_TEXT));
+//        // track the position of the item in the list
+//        position = getIntent().getIntExtra(ITEM_POSITION, 0);
     }
 }
